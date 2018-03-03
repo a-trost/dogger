d0d6fce3be059636359d3c5f2ea63139349d34bc
Alexander Trost <AlexRTrost@gmail.com>
2018-03-03 18:46:34 -0500

    feat: Move texture paths to object

---
+++ js/textures.js
@@ -0,0 +1,87 @@
+const worldTextures = {
+    'House':[
+        // Player Textures
+        'images/dog-u.png',
+        'images/dog-d.png',
+        'images/dog-l.png',
+        'images/dog-r.png',
+        // Floors
+        'images/block-carpet1.png',
+        'images/block-carpet2.png',
+        'images/block-carpet3.png',
+        'images/block-kitchen.png',
+        'images/block-grass.png',
+        'images/block-finish.png',
+        // Enemies
+        'images/roomba-l.png',
+        'images/roomba-r.png',
+        'images/hamster-r.png',
+        'images/hamster-l.png',
+        'images/cat-l.png',
+        'images/cat-r.png',
+        // Barriers
+        'images/washer.png',
+        'images/tv.png',
+        'images/toaster.png',
+        'images/chair.png',
+        // Prize
+        'images/hotdog.png',
+    ],
+
+    'Forest':[
+        // Player Textures
+        'images/dog-u.png',
+        'images/dog-d.png',
+        'images/dog-l.png',
+        'images/dog-r.png',
+        // Floors
+        'images/block-grass.png',
+        'images/block-finish.png',
+        // Enemies
+        'images/ram-l.png',
+        'images/ram-r.png',
+        'images/sloth-l.png',
+        'images/sloth-r.png',
+        'images/fox-l.png',
+        'images/fox-r.png',
+        'images/deer-l.png',
+        'images/deer-r.png',
+        'images/ferret-l.png',
+        'images/ferret-r.png',
+        // Barriers
+        'images/rock1.png',
+        'images/rock2.png',
+        'images/rock3.png',
+        'images/rock4.png',
+        'images/tree1.png',
+        // Prize
+        'images/hotdog.png',
+    ],
+    'City':[
+        // Player Textures
+        'images/dog-u.png',
+        'images/dog-d.png',
+        'images/dog-l.png',
+        'images/dog-r.png',
+        // Floors
+        'images/block-grass.png',
+        'images/block-finish.png',
+        'images/block-road-l.png',
+        'images/block-road-r.png',
+        // Enemies
+        'images/carr-r.png',
+        'images/carr-l.png',
+        'images/carb-r.png',
+        'images/carb-l.png',
+        'images/vanb-r.png',
+        'images/vanb-l.png',
+        'images/vanw-r.png',
+        'images/vanw-l.png',
+        'images/vang-r.png',
+        'images/vang-l.png',
+        // Barriers
+
+        // Prize
+        'images/hotdog.png',
+    ],
+}
